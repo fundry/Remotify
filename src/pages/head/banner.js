ff6afbbaceeed16f8401a5d423d25d718b16c6dc
Nwani Victory <vickywane@gmail.com>
2019-06-16 08:32:07 +0100

    Created Basic Components

---
+++ src/pages/head/banner.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Flex from "styled-flex-component"
+import styled from "styled-components"
+
+import Header from "./header"
+
+const Banner = () => {
+  const Div = styled.div``
+
+  return (
+    <Div>
+      <Header />
+      <h1>Header</h1>
+    </Div>
+  )
+}
+
+export default Banner
---
+++ src/pages/head/header.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Flex from "styled-flex-component"
+import styled from "styled-components"
+
+const Header = () => {
+  const Div = styled.div``
+
+  return (
+    <Div>
+      <h1>Header</h1>
+    </Div>
+  )
+}
+
+export default Header
