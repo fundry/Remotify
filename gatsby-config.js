79cd47cd76bf560d3c46199f76036601bfc3a584
Nwani Victory <vickywane@gmail.com>
2019-06-17 01:02:05 +0100

    Fixed Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Remotify`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
