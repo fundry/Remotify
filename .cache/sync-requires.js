d4fa143316723dcb272bb5b5345f5c50386da8e7
Nwani Victory <vickywane@gmail.com>
2019-07-10 13:39:39 +0100

    Added hooks style

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -10,11 +10,11 @@ exports.components = {
   "component---src-pages-footer-footer-js": hot(preferDefault(require("C:\\Users\\Iamnwani\\Desktop\\ROJECT\\Remotify\\src\\pages\\footer\\footer.js"))),
   "component---src-pages-head-banner-js": hot(preferDefault(require("C:\\Users\\Iamnwani\\Desktop\\ROJECT\\Remotify\\src\\pages\\head\\banner.js"))),
   "component---src-pages-head-header-js": hot(preferDefault(require("C:\\Users\\Iamnwani\\Desktop\\ROJECT\\Remotify\\src\\pages\\head\\header.js"))),
+  "component---src-pages-home-features-js": hot(preferDefault(require("C:\\Users\\Iamnwani\\Desktop\\ROJECT\\Remotify\\src\\pages\\home\\features.js"))),
   "component---src-pages-home-home-js": hot(preferDefault(require("C:\\Users\\Iamnwani\\Desktop\\ROJECT\\Remotify\\src\\pages\\home\\home.js"))),
   "component---src-pages-home-integrate-js": hot(preferDefault(require("C:\\Users\\Iamnwani\\Desktop\\ROJECT\\Remotify\\src\\pages\\home\\integrate.js"))),
   "component---src-pages-home-power-js": hot(preferDefault(require("C:\\Users\\Iamnwani\\Desktop\\ROJECT\\Remotify\\src\\pages\\home\\power.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Iamnwani\\Desktop\\ROJECT\\Remotify\\src\\pages\\index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("C:\\Users\\Iamnwani\\Desktop\\ROJECT\\Remotify\\src\\pages\\page-2.js"))),
-  "component---src-pages-home-features-js": hot(preferDefault(require("C:\\Users\\Iamnwani\\Desktop\\ROJECT\\Remotify\\src\\pages\\home\\features.js")))
+  "component---src-pages-page-2-js": hot(preferDefault(require("C:\\Users\\Iamnwani\\Desktop\\ROJECT\\Remotify\\src\\pages\\page-2.js")))
 }
 
--- src/pages/home/features.js
+++ src/pages/home/features.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Flex from 'styled-flex-component';
 import Sample from './svg/console_sample.svg';
 import posed from 'react-pose';
+import useWindowWidth from '../../hook_style';
 
 const features = () => {
   const Div = {
@@ -26,64 +27,133 @@ const features = () => {
     },
   });
 
+  const hooks = useWindowWidth();
+
   return (
-    <div style={Div}>
-      <Flex justifyCenter>
-        <div style={{ padding: '2em' }}>
-          <h3 style={{ textAlign: 'center' }}> Customizable Interface </h3>
-          <p style={{ textAlign: 'center' }}>
-            Switch between colors and lyouts to give the unique development
-            environemt.
-          </p>{' '}
+    <div>
+      {hooks >= 1000 ? (
+        <div style={Div}>
           <Flex justifyCenter>
+            <div style={{ padding: '2em', width: '70%' }}>
+              <h2 style={{ textAlign: 'center' }}> Customizable Interface </h2>
+              <p style={{ textAlign: 'center', fontSize: '1.2em' }}>
+                Switch between colors and layouts to give the unique development
+                environemt.
+              </p>
+            </div>
             <Hover>
-              <img src={Sample} style={{ maxWidth: '75%' }} />{' '}
+              <img src={Sample} style={{ maxWidth: '70%' }} />{' '}
             </Hover>
           </Flex>
-        </div>
-      </Flex>
-      <Flex justifyCenter>
-        <div style={{ padding: '2em' }}>
-          <h3 style={{ textAlign: 'center' }}> Integrate External Tools </h3>
-          <p style={{ textAlign: 'center' }}>
-            Switch between colors and lyouts to give the unique development
-            environemt.
-          </p>{' '}
           <Flex justifyCenter>
             <Hover>
-              <img src={Sample} style={{ maxWidth: '75%' }} />{' '}
+              <img src={Sample} style={{ maxWidth: '70%' }} />{' '}
+            </Hover>
+            <div style={{ padding: '2em', width: '70%' }}>
+              <h2 style={{ textAlign: 'center' }}>
+                {' '}
+                Integrate External Tools{' '}
+              </h2>
+              <p style={{ textAlign: 'center', fontSize: '1.2em' }}>
+                Switch between colors and layouts to give the unique development
+                environemt.
+              </p>
+            </div>
+          </Flex>
+          <Flex justifyCenter>
+            <div style={{ padding: '2em', width: '70%' }}>
+              <h2 style={{ textAlign: 'center' }}>
+                {' '}
+                Monitor working performance {' '}
+              </h2>
+              <p style={{ textAlign: 'center', fontSize: '1.2em' }}>
+                Switch between colors and layouts to give the unique development
+                environemt.
+              </p>
+            </div>
+            <Hover>
+              <img src={Sample} style={{ maxWidth: '70%' }} />{' '}
             </Hover>
           </Flex>
-        </div>
-      </Flex>
-      <Flex justifyCenter>
-        <div style={{ padding: '2em' }}>
-          <h3 style={{ textAlign: 'center' }}> Monitor working performance </h3>
-          <p style={{ textAlign: 'center' }}>
-            Switch between colors and lyouts to give the unique development
-            environemt.
-          </p>{' '}
           <Flex justifyCenter>
             <Hover>
-              <img src={Sample} style={{ maxWidth: '75%' }} />{' '}
+              <img src={Sample} style={{ maxWidth: '70%' }} />{' '}
             </Hover>
+            <div style={{ padding: '2em', width: '70%' }}>
+              <h2 style={{ textAlign: 'center' }}> Track working hours </h2>
+              <p style={{ textAlign: 'center', fontSize: '1.2em' }}>
+                Switch between colors and layouts to give the unique development
+                environemt.
+              </p>
+            </div>
           </Flex>
         </div>
-      </Flex>{' '}
-      <Flex justifyCenter>
-        <div style={{ padding: '2em' }}>
-          <h3 style={{ textAlign: 'center' }}> Track working hours.</h3>
-          <p style={{ textAlign: 'center' }}>
-            Switch between colors and lyouts to give the unique development
-            environemt.
-          </p>{' '}
+      ) : (
+        <div style={Div}>
           <Flex justifyCenter>
-            <Hover>
-              <img src={Sample} style={{ maxWidth: '75%' }} />{' '}
-            </Hover>
+            <div style={{ padding: '2em' }}>
+              <h3 style={{ textAlign: 'center' }}> Customizable Interface </h3>
+              <p style={{ textAlign: 'center', fontSize: '1.1em' }}>
+                Switch between colors and layouts to create your unique working
+                environemt environemt.
+              </p>{' '}
+              <Flex justifyCenter>
+                <Hover>
+                  <img src={Sample} style={{ maxWidth: '75%' }} />{' '}
+                </Hover>
+              </Flex>
+            </div>
+          </Flex>
+          <Flex justifyCenter>
+            <div style={{ padding: '2em' }}>
+              <h3 style={{ textAlign: 'center' }}>
+                {' '}
+                Integrate External Tools{' '}
+              </h3>
+              <p style={{ textAlign: 'center', fontSize: '1.1em' }}>
+                Switch between colors and lyouts to give the unique development
+                environemt.
+              </p>{' '}
+              <Flex justifyCenter>
+                <Hover>
+                  <img src={Sample} style={{ maxWidth: '75%' }} />{' '}
+                </Hover>
+              </Flex>
+            </div>
+          </Flex>
+          <Flex justifyCenter>
+            <div style={{ padding: '2em' }}>
+              <h3 style={{ textAlign: 'center' }}>
+                {' '}
+                Monitor working performance{' '}
+              </h3>
+              <p style={{ textAlign: 'center', fontSize: '1.1em' }}>
+                Switch between colors and lyouts to give the unique development
+                environemt.
+              </p>{' '}
+              <Flex justifyCenter>
+                <Hover>
+                  <img src={Sample} style={{ maxWidth: '75%' }} />{' '}
+                </Hover>
+              </Flex>
+            </div>
+          </Flex>{' '}
+          <Flex justifyCenter>
+            <div style={{ padding: '2em' }}>
+              <h3 style={{ textAlign: 'center' }}> Track working hours.</h3>
+              <p style={{ textAlign: 'center', fontSize: '1.1em' }}>
+                Switch between colors and lyouts to give the unique development
+                environemt.
+              </p>{' '}
+              <Flex justifyCenter>
+                <Hover>
+                  <img src={Sample} style={{ maxWidth: '75%' }} />{' '}
+                </Hover>
+              </Flex>
+            </div>
           </Flex>
         </div>
-      </Flex>
+      )}
     </div>
   );
 };
