b8f1945848165617933847eae899bc43463d774b
Nwani Victory <vickywane@gmail.com>
2019-11-14 19:59:15 +0100

    ..

--- src/pages/home/components/storage.js
+++ src/pages/home/components/storage.js
@@ -1,28 +1,54 @@
-import React from "react"
-import styled from "styled-components"
-import Flex from "styled-flex-component"
-import media from "styled-media-query"
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Flex from 'styled-flex-component';
+import media from 'styled-media-query';
 
 const Storage = () => {
-  const Head = styled.h2``
-  const Subhead = styled.h4``
+  const Head = styled.h2``;
+  const Subhead = styled.h4``;
 
   const Body = styled.div`
     padding: 1em;
     color: #361f94;
-    ${media.lessThan("medium")`
+    ${media.lessThan('medium')`
       paddingLeft : 5em
     `};
-  `
+  `;
+
+  const [Width, setWidth] = useState(null);
+
+  setTimeout(function() {
+    setWidth(window.innerWidth);
+  }, 100);
+
+  const handleResize = (value) => {
+    setWidth(value);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize.bind(this));
+    return () => window.removeEventListener('resize', handleResize.bind(this));
+  }, []);
+
+  const HeadTextBody = styled.div`
+    padding-left: 10em ${media.lessThan('medium')`
+      padding-left : 1.5em
+    `};
+    ${media.lessThan('large')`
+      padding-left : 7em
+    `};
+  `;
 
   return (
     <Body>
       <Flex column>
-        <Head>Cloud Storage.</Head>
-        <Subhead>For your assets</Subhead>
+        <HeadTextBody>
+          <Head style={{ fontWeight: 'bold' }}>Cloud Storage.</Head>
+          <Subhead>For your assets</Subhead>
+        </HeadTextBody>
       </Flex>
     </Body>
-  )
-}
+  );
+};
 
-export default Storage
+export default Storage;
